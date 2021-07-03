e65d032c9e3c9429624675342c9473708947a946
Petr <superdytko@gmail.com>
2021-07-03 14:01:17 +0300

    test

--- yamdb/serializers.py
+++ yamdb/serializers.py
@@ -1,6 +1,5 @@
-from rest_framework import serializers
-
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 
 from .models import Category, Comment, Genre, Review, Title
 
